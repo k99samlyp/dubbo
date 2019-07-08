e836cadc4d1412061b6a9d74cb386d76fb772cf2
sam <317772420@qq.com>
2019-07-08 22:23:56 +0800

    服务端完成

--- src/main/java/com/uvb76/dubbo/DubboApplication.java
+++ src/main/java/com/uvb76/dubbo/DubboApplication.java
@@ -2,12 +2,24 @@ package com.uvb76.dubbo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-@SpringBootApplication
+import java.io.IOException;
+
+//@SpringBootApplication(exclude = {HibernateJpaAutoConfiguration.class})
 public class DubboApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(DubboApplication.class, args);
+    public static void main(String[] args) throws IOException {
+        //SpringApplication.run(DubboApplication.class, args);
+
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(new String[]{"provider.xml"});
+
+        context.start();
+
+        System.out.println("启动完成 ");
+
+        System.in.read();
     }
 
 }
---
+++ src/main/java/com/uvb76/dubbo/service/RadioServiceImpl.java
@@ -0,0 +1,9 @@
+package com.uvb76.dubbo.service;
+
+public class RadioServiceImpl implements RadioService {
+
+    @Override
+    public String radioOn() {
+        return "uvb76  uvb76 999 !!!!";
+    }
+}
